a4c1f03601aa20ab4cfce4b5802520da21e79676
Cithoreal <cdicarlo28@gmail.com>
2022-10-31 01:20:14 -0400

    Improved links for future use

--- LineRenderer.gd
+++ LineRenderer.gd
@@ -4,21 +4,24 @@ extends ImmediateGeometry
 var m = SpatialMaterial.new()
 
 var show_path = true
-export var this_bubble : NodePath
-export var linked_bubble : NodePath
+export var bubble1 : NodePath
+export var bubble2 : NodePath
+export var link_color : Color
 var path = []
 # Called when the node enters the scene tree for the first time.
 func _enter_tree():
 	set_process_input(true)
 	m.flags_unshaded = true
 	m.flags_use_point_size = true
-	m.albedo_color = Color.blue
+	m.albedo_color = link_color
 
 
 func _process(_delta):
+	if bubble1 == "" or bubble2 == "":
+		return
 	path.clear()
-	path.append(get_node(this_bubble).transform.origin) 
-	path.append(get_node(linked_bubble).transform.origin)
+	path.append(get_node(bubble1).transform.origin) 
+	path.append(get_node(bubble2).transform.origin)
 	draw_path(path)
 
 func draw_path(path_array):
