0cd0e903f9624536383af4da15d26b400bfdda21
Cithoreal <cdicarlo28@gmail.com>
2022-10-30 23:19:23 -0400

    Added sufficient line rendering

--- LineRenderer.gd
+++ LineRenderer.gd
@@ -1,30 +1,34 @@
 tool
-extends Node
+extends ImmediateGeometry
 
 var m = SpatialMaterial.new()
 
-var path = []
 var show_path = true
-
+export var this_bubble : NodePath
+export var linked_bubble : NodePath
+var path = []
 # Called when the node enters the scene tree for the first time.
 func _enter_tree():
 	set_process_input(true)
 	m.flags_unshaded = true
 	m.flags_use_point_size = true
-	m.albedo_color = Color.red
-	path.append(Vector3(-1,0,0)) 
-	path.append(Vector3(0,1,0))
+	m.albedo_color = Color.blue
+
+
+func _process(_delta):
+	path.clear()
+	path.append(get_node(this_bubble).transform.origin) 
+	path.append(get_node(linked_bubble).transform.origin)
 	draw_path(path)
 
 func draw_path(path_array):
-	var im = get_node("Draw")
-	im.set_material_override(m)
-	im.clear()
-	im.begin(Mesh.PRIMITIVE_POINTS, null)
-	im.add_vertex(path_array[0])
-	im.add_vertex(path_array[path_array.size() - 1])
-	im.end()
-	im.begin(Mesh.PRIMITIVE_LINE_STRIP, null)
+	set_material_override(m)
+	clear()
+	begin(Mesh.PRIMITIVE_POINTS, null)
+	add_vertex(path_array[0])
+	add_vertex(path_array[path_array.size() - 1])
+	end()
+	begin(Mesh.PRIMITIVE_LINE_STRIP, null)
 	for x in path:
-		im.add_vertex(x)
-	im.end()
+		add_vertex(x)
+	end()
