cfa25759f775ce8e65743399cf992677bfd3dfce
Cithoreal <cdicarlo28@gmail.com>
2022-11-19 00:33:10 -0500

    Prepping Godot - MB pipeline

--- Scripts/ThoughtBubble.gd
+++ Scripts/ThoughtBubble.gd
@@ -3,21 +3,20 @@ extends Spatial
 
 export(bool) var button setget _button
 export(Array, NodePath) var linkedThoughts  
+var thoughtbubbles_path = "Thought_Space/----Thought Bubbles----"
+var thoughtspace_path = "Thought_Space"
+onready var thoughtspace = get_node("/root/EditorNode/@@596/@@597/@@605/@@607/@@611/@@615/@@616/@@617/@@633/@@634/@@643/@@644/@@6618/@@6450/@@6451/@@6452/@@6453/@@6454/@@6455/root/Thought_Space")
 
 func _button(_value):
 	print("button pressed")
 	
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	get_child(0).text=get_name()
+	thoughtspace.connect("save_thoughts" , self, "_on_save_thoughts")
+	#Lookup self in the memory base, exit if doesn't already exist
+	#If it does exist, collect all properties/meta values and apply them to self
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _input(_event):
-	pass
+func _on_save_thoughts():
+	print(get_name() + " is saving")
+	#Collect all meta properties
+	#execute external python script and pass it the node name and each property
--- Scripts/ThoughtSpace.gd
+++ Scripts/ThoughtSpace.gd
@@ -3,41 +3,60 @@ extends Spatial
 
 export(bool) var load_collection setget _start_recall
 export(bool) var save_thoughts setget _save
-export var recall_thought = ""
+export var thought_collection = ""
+export var new_thought = ""
+export(bool) var create_new_thought setget _create_new_thought
 var MB_to_godot_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/MB_to_godot.py"
 var godot_to_nodes_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/godot_to_nodes.py"
+var thought_scene = load("res://ThoughtBubble.tscn")
 
-var links_path = "Links"
-var thoughtbubbles_path = "----Thought Bubbles----"
+var links_path = "Thought_Space/Links"
+var thoughtbubbles_path = "Thought_Space/----Thought Bubbles----"
+
+signal save_thoughts
+signal clear_thoughts
 
 func _start_recall(_value):
-	if (recall_thought != ""):
+	if (thought_collection != ""):
 		var output = []
-		OS.execute(MB_to_godot_path, [recall_thought], true, output)
+		OS.execute(MB_to_godot_path, [thought_collection], true, output)
 		process_thoughts(output)
 
 func _save(_value):
-	if (recall_thought != ""):
-		print("saving thoughts")
-		OS.execute(godot_to_nodes_path, [], false)
+	print("saving thoughts")
+	emit_signal("save_thoughts")
+	#OS.execute(godot_to_nodes_path, [], false)
 
+#Prep the scene to be loaded with new thoughts
 func clear_scene():
+	emit_signal("clear_thoughts")
 	for link in get_node(links_path).get_children():
 		link.queue_free()
 	for node in get_node(thoughtbubbles_path).get_children():
 		node.queue_free()
 	pass
-	#find all LineRender and ThoughtBubble scripts and remove nodes they are attached to 
 
+#Use the text retrieved from _start_recall to retrieve thoughts and thought properties
 func process_thoughts(text_block):
 	#Retrieve a union of the thought and "Transform" to recieve the transform numbers
 	for i in text_block[0].split(")),"):
 		var linked_node = i.split("',")[2].replace(" '","")
 		print(linked_node)
 	
-
+#Instantiate a Thought Bubble node into the thought space
 func load_thought(thought, transform):
 	print (thought + " " + transform)
-	
+
+#Instantiate links between thoughts
 func load_links():
 	pass
+	
+func _create_new_thought(_value):
+	if new_thought != "":
+		print("Creating new thought: " + new_thought)
+		var new_bubble = thought_scene.instance()
+		new_bubble.set_name(new_thought)
+		get_node(thoughtbubbles_path).add_child(new_bubble)
+		new_bubble.set_owner(self)
+		
+		
