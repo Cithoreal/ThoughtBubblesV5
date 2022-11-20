95e6249d91030ce1926ae9ff2d81c910795e3567
Cithoreal <cdicarlo28@gmail.com>
2022-11-20 15:24:39 -0500

    Basic Saving and Loading

--- Scripts/ThoughtBubble.gd
+++ Scripts/ThoughtBubble.gd
@@ -2,21 +2,76 @@ tool
 extends Spatial
 
 export(bool) var button setget _button
-export(Array, NodePath) var linkedThoughts  
-var thoughtbubbles_path = "Thought_Space/----Thought Bubbles----"
-var thoughtspace_path = "Thought_Space"
-onready var thoughtspace = get_node("/root/EditorNode/@@596/@@597/@@605/@@607/@@611/@@615/@@616/@@617/@@633/@@634/@@643/@@644/@@6618/@@6450/@@6451/@@6452/@@6453/@@6454/@@6455/root/Thought_Space")
+export(Array, String) var linkedThoughts  
+var MB_to_godot_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/MB_to_godot.py"
+var godot_to_nodes_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/godot_to_nodes.py"
+onready var thoughtspace_node = get_parent().get_parent()
 
 func _button(_value):
 	print("button pressed")
 	
 func _ready():
+	
 	get_child(0).text=get_name()
-	thoughtspace.connect("save_thoughts" , self, "_on_save_thoughts")
+	thoughtspace_node.connect("save_thoughts" , self, "_on_save_thoughts")
+	#load_thoughts()
 	#Lookup self in the memory base, exit if doesn't already exist
 	#If it does exist, collect all properties/meta values and apply them to self
+	
+func load_thoughts():
+	print("Loading " + get_name())
+	load_position()
+	load_links()
 
+func load_position():
+	var output = []
+	var x = ""
+	var y = ""
+	var z = ""
+	OS.execute(MB_to_godot_path, [get_name(), "|Position|", "|x|"], true, output)
+	x = output[0].replace("[('", "")
+	x = x.replace("',)]", "")
+	OS.execute(MB_to_godot_path, [get_name(), "|Position|", "|y|"], true, output)
+	y = output[0].replace("[('", "")
+	y = y.replace("',)]", "")
+	OS.execute(MB_to_godot_path, [get_name(), "|Position|", "|z|"], true, output)
+	z = output[0].replace("[('", "")
+	z = z.replace("',)]", "")
+	transform.origin = Vector3(float(x),float(y),float(z))
+	
+	
+func load_links():
+	var output = []
+	OS.execute(MB_to_godot_path, [get_name(), "|Link|"], true, output)
+	var text = output[0].replace("[(", "")
+	text = text.replace(",)]", "")
+	text = text.replace("\n", "")
+	for i in text.split(",), ("):
+		var linked_node = i.replace("'","")
+		if (linked_node != "[]"):
+			linkedThoughts.append(linked_node)
+	
 func _on_save_thoughts():
-	print(get_name() + " is saving")
+	# Name
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Thought|", "|Text|", get_name()], false)
+	# Position
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Position|", "|x|", str(transform.origin.x)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Position|", "|y|", str(transform.origin.y)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Position|", "|z|", str(transform.origin.z)], false)
+	# Basis
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|xx|", str(transform.basis.x.x)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|xy|", str(transform.basis.x.y)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|xz|", str(transform.basis.x.z)], false)
+	
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|yx|", str(transform.basis.y.x)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|yy|", str(transform.basis.y.y)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|yz|", str(transform.basis.y.z)], false)
+	
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|zx|", str(transform.basis.z.x)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|zy|", str(transform.basis.z.y)], false)
+	OS.execute(godot_to_nodes_path, ["|Godot|", "|Bubble|", get_name(), "|Transform|", "|Basis|", "|zz|", str(transform.basis.z.z)], false)
+	
+	for link in linkedThoughts:
+		OS.execute(godot_to_nodes_path, ["Godot", get_name(), "|Link|", str(link).replace("../", "")], false)
 	#Collect all meta properties
 	#execute external python script and pass it the node name and each property
--- Scripts/ThoughtSpace.gd
+++ Scripts/ThoughtSpace.gd
@@ -5,58 +5,62 @@ export(bool) var load_collection setget _start_recall
 export(bool) var save_thoughts setget _save
 export var thought_collection = ""
 export var new_thought = ""
-export(bool) var create_new_thought setget _create_new_thought
+export(bool) var create_new_thought setget _on_new_thought_button
+export(bool) var run_functions = false
 var MB_to_godot_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/MB_to_godot.py"
 var godot_to_nodes_path = "/run/media/cithoreal/Elements/MemoryBase/ToThoughts-Git/godot_to_nodes.py"
 var thought_scene = load("res://ThoughtBubble.tscn")
-
-var links_path = "Thought_Space/Links"
-var thoughtbubbles_path = "Thought_Space/----Thought Bubbles----"
 
 signal save_thoughts
 signal clear_thoughts
 
 func _start_recall(_value):
-	if (thought_collection != ""):
+	if (len(thought_collection) > 0 && run_functions):
 		var output = []
-		OS.execute(MB_to_godot_path, [thought_collection], true, output)
-		process_thoughts(output)
+		OS.execute(MB_to_godot_path, [thought_collection, "|Thought|"], true, output)
+		#process_thoughts(output)
 
 func _save(_value):
-	print("saving thoughts")
-	emit_signal("save_thoughts")
-	#OS.execute(godot_to_nodes_path, [], false)
+	if (run_functions):
+		print("saving thoughts")
+		emit_signal("save_thoughts")
+		#OS.execute(godot_to_nodes_path, [], false)
 
 #Prep the scene to be loaded with new thoughts
 func clear_scene():
 	emit_signal("clear_thoughts")
-	for link in get_node(links_path).get_children():
-		link.queue_free()
-	for node in get_node(thoughtbubbles_path).get_children():
-		node.queue_free()
+	for link in get_child(0).get_children():
+		link.free()
+	for node in get_child(1).get_children():
+		node.free()
 	pass
 
 #Use the text retrieved from _start_recall to retrieve thoughts and thought properties
 func process_thoughts(text_block):
-	#Retrieve a union of the thought and "Transform" to recieve the transform numbers
-	for i in text_block[0].split(")),"):
-		var linked_node = i.split("',")[2].replace(" '","")
-		print(linked_node)
-	
-#Instantiate a Thought Bubble node into the thought space
-func load_thought(thought, transform):
-	print (thought + " " + transform)
+	clear_scene()
+	var text = text_block[0].replace("[(", "")
+	text = text.replace(",)]", "")
+	text = text.replace("\n", "")
+	#Retrieve an intersect of the thought and "Transform" to recieve the transform numbers
+	print("Creating... " + thought_collection)
+	create_new_thought(thought_collection)
+	for i in text.split(",), ("):
+		var linked_node = i.replace("'","")
+		print("Creating... " + linked_node)
+		create_new_thought(linked_node)
 
 #Instantiate links between thoughts
 func load_links():
 	pass
+
+func _on_new_thought_button(_value):
+	if (new_thought != "" && run_functions):
+		create_new_thought(new_thought)
 	
-func _create_new_thought(_value):
-	if new_thought != "":
-		print("Creating new thought: " + new_thought)
+func create_new_thought(thought_text):
+	if thought_text != "" && run_functions:
 		var new_bubble = thought_scene.instance()
-		new_bubble.set_name(new_thought)
-		get_node(thoughtbubbles_path).add_child(new_bubble)
-		new_bubble.set_owner(self)
-		
+		new_bubble.set_name(thought_text)
+		get_child(1).add_child(new_bubble)
+		new_bubble.set_owner(get_parent())
 		
