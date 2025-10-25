a7b92b2437ec6db6d0fbe68e6180d14ec6fd3264
Chris <syrhicalacid@gmail.com>
2025-10-25 12:17:44 -0400

    refactored loading functions to thoughtbubble_store.gd

--- Scripts/FileManager.gd
+++ Scripts/FileManager.gd
@@ -3,7 +3,7 @@ extends Node
 
 class_name FileManager
 
-var FILE_PATH = "res://Files/"
+const FILE_PATH = "res://Files/Thoughts/"
 
 # No need to extend, just use FileManager and save to ndjson and jsonld
 # Saving to an incremental log, individual thought files, and potentially 
@@ -16,26 +16,13 @@ var FILE_PATH = "res://Files/"
 #Load()         loads file into cache
 #Get(args[])    Get subset from cache
 
-
-@export var FILE_NAME = "thought_dictionary"
-var FILE_SUFFIX = ".json"
-
-func save(data):
-	print("Saving data to JSON")
-	print(data)
-	var file_path = FILE_PATH + FILE_NAME + FILE_SUFFIX
-	print(file_path)
-	var json_string = JSON.stringify(data)
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	file.store_string(json_string)
-	file.close()
-	#save_file(json_string, FILE_PATH + FILE_NAME + FILE_SUFFIX)
 	
-func save_jsonld(data_dict, sub_folder): # for thoughtbubbles #just get and update existing files with new timestamp
+func save_jsonld(data_dict): # for thoughtbubbles #just get and update existing files with new timestamp
 	print(data_dict)
-	if not DirAccess.dir_exists_absolute(FILE_PATH + sub_folder +"/"):
-		DirAccess.make_dir_absolute(FILE_PATH + sub_folder +"/")
-	var file_path = FILE_PATH + sub_folder +"/"+data_dict["@id"] + ".jsonld"
+	if not DirAccess.dir_exists_absolute(FILE_PATH):
+		DirAccess.make_dir_absolute(FILE_PATH)
+	var file_path = FILE_PATH + data_dict["@id"] + ".jsonld"
+	print("FILEPATH ", FILE_PATH)
 	var file_exists = FileAccess.file_exists(file_path)
 	var json_string :String
 
@@ -88,7 +75,7 @@ func array_to_dict(array):
 	return out_dict
 
 func load_jsonld(load_target):
-	var file_path = FILE_PATH + "Thoughts/" + load_target + ".jsonld"
+	var file_path = FILE_PATH  + load_target + ".jsonld"
 	print("loading from file Path:" + file_path)
 
 	var obj = open_json_file(file_path)
@@ -97,14 +84,15 @@ func load_jsonld(load_target):
 	return obj
 
 func get_latest_timestamp(thought_id: String):
-	var file_path = FILE_PATH + "Thoughts/" + thought_id + ".jsonld"
-	print("loading from file Path:" + file_path)
-
+	var file_path = FILE_PATH + thought_id + ".jsonld"
+	print("loading from file Path: " + file_path)
+	print(open_json_file(file_path))
 	return open_json_file(file_path)["lastUpdated"]
 
 func open_json_file(file_path):
 	if not FileAccess.file_exists(file_path):
-		return ""
+		push_warning("No file at location: ", file_path)
+		return "No File"
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	var file_contents = file.get_as_text()
 	file.close()
@@ -113,7 +101,7 @@ func open_json_file(file_path):
 	var err = json.parse(file_contents)
 	if (err != OK):
 		push_warning("JSON parse failed in %s: %s" % [file_path, json.get_error_message()])
-		return ""
+		return "Invalid JSON"
 	else:
 		var obj = json.get_data()
 		return obj
